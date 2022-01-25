f0ab24f9b13be621845c94d739a6d66b3b55ef11
김우찬 <woot5785@naver.com>
2022-01-25 16:14:24 +0900

    :tada: Study, StudyMember entitiy add

--- backend/src/main/java/com/ssafy/db/entity/Study.java
+++ backend/src/main/java/com/ssafy/db/entity/Study.java
@@ -1,10 +1,16 @@
 package com.ssafy.db.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -31,4 +37,9 @@ public class Study {
 	String interests;
 	@Column
 	String timestamp;
+	
+	
+	@OneToMany
+	@JoinColumn(name = "study_id")
+	List<StudyMember> studyMemberList = new ArrayList<StudyMember>();
 }
---
+++ backend/src/main/java/com/ssafy/db/entity/StudyMember.java
@@ -0,0 +1,43 @@
+package com.ssafy.db.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "study_member")
+@Getter
+@Setter
+@IdClass(StudyMemberId.class)
+public class StudyMember implements Serializable{
+	@Id
+	int userno;
+	@Id
+	int studyno;
+	@Column
+	int position;
+	@Column(name = "study_time")
+	String studyTime;
+	@Column(name = "is_join")
+	int isJoin;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "study_id")
+	Study study;
+}
