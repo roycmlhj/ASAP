eb15b2063bc633cf4ba95b3557b5229e85db07f1
김우찬 <woot5785@naver.com>
2022-01-25 16:24:20 +0900

    :tada: StudyMember entitiy Composite key setting file add

---
+++ backend/src/main/java/com/ssafy/db/entity/StudyMemberId.java
@@ -0,0 +1,15 @@
+package com.ssafy.db.entity;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class StudyMemberId implements Serializable {
+	private int userno;
+	private int studyno;
+}
