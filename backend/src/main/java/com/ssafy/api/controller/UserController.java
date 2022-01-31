1678f93f82f1de721654fe9e962f7b581a5e1ec8
kwwoo516 <kwwoo516@naver.com>
2022-02-01 01:52:27 +0900

    :sparkles: add Email confirm

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -21,6 +21,7 @@ import com.ssafy.api.request.UserRegisterPostReq;
 import com.ssafy.api.response.UserDetailInfoRes;
 import com.ssafy.api.response.UserListRes;
 import com.ssafy.api.response.UserLoginPostRes;
+import com.ssafy.api.service.EmailService;
 import com.ssafy.api.service.StudyService;
 import com.ssafy.api.service.UserService;
 import com.ssafy.common.model.response.BaseResponseBody;
@@ -47,7 +48,8 @@ public class UserController {
 	UserService userService;
 	@Autowired
 	StudyService studyService;
-	
+	@Autowired
+	EmailService emailService;
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	
@@ -104,7 +106,22 @@ public class UserController {
 			return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));	
 		}
 		return ResponseEntity.status(404).body(BaseResponseBody.of(404, "이미 존재하는 이메일입니다."));
-	}	
+	}
+	
+	@PostMapping("emailConfirm")
+	@ApiOperation(value = "회원 가입시 이메인 인증", notes = "기존사용하고 있는 이메일을 통해 인증")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+	public ResponseEntity<? extends BaseResponseBody> emailConfirm(
+			@RequestBody @ApiParam(value="이메일 정보", required = true) String email) throws Exception{
+		String confirm = emailService.sendSimpleMessage(email);
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, confirm));
+	}
+	
 	
 	@PostMapping("/{userno}")
 	@ApiOperation(value = "회원정보 수정", notes = "회원의 정보를 수정한다.") 
